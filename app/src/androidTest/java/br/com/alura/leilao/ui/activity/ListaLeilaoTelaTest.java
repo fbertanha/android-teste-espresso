4d9b96b224aa4cdf5b5a4b78c71b244177e544e8
felipe.bertanha <felipe.bertanha@capitaletecnologia.com.br>
2020-07-12 12:11:45 -0300

    aula 2

---
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -0,0 +1,45 @@
+package br.com.alura.leilao.ui.activity;
+
+import android.content.Intent;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.model.Leilao;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by felipebertanha on 06/July/2020
+ */
+public class ListaLeilaoTelaTest {
+
+    @Rule
+    public ActivityTestRule<ListaLeilaoActivity> activity =
+            new ActivityTestRule<>(ListaLeilaoActivity.class, true, false);
+
+    @Test
+    public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaApi() throws IOException {
+        Leilao leilao = new LeilaoWebClient().salva(new Leilao("Carro"));
+
+        if (leilao == null) {
+            Assert.fail("Leilão não foi salvo");
+        }
+
+        activity.launchActivity(new Intent());
+
+        onView(withText("Carro"))
+                .check(matches(isDisplayed()));
+    }
+}
