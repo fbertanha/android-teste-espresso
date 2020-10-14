ee2808cb1bbde6aa209ee4e72192a41a76831ab9
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2020-10-13 23:10:13 -0300

    aula 2.4 - refactoring pt 2

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaUsuariosTelaTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaUsuariosTelaTest.java
@@ -3,22 +3,12 @@ package br.com.alura.leilao.ui.activity;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import br.com.alura.leilao.BuildConfig;
 import br.com.alura.leilao.R;
@@ -52,52 +42,27 @@ public class ListaUsuariosTelaTest {
                 isDisplayed()))
                 .perform(click());
 
-        ViewInteraction floatingActionButton = onView(
-                allOf(withId(R.id.lista_usuario_fab_adiciona),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        floatingActionButton.perform(click());
+        onView(allOf(withId(R.id.lista_usuario_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
 
-        ViewInteraction textInputEditText = onView(
-                allOf(childAtPosition(
-                        childAtPosition(
-                                withId(R.id.form_usuario_nome),
-                                0),
-                        0),
-                        isDisplayed()));
-        textInputEditText.perform(click());
+        onView(allOf(withId(R.id.form_usuario_nome),
+                isDisplayed()))
+                .perform(click());
 
-        ViewInteraction textInputEditText2 = onView(
-                allOf(childAtPosition(
-                        childAtPosition(
-                                withId(R.id.form_usuario_nome),
-                                0),
-                        0),
-                        isDisplayed()));
-        textInputEditText2.perform(replaceText("Felipe"), closeSoftKeyboard());
+        onView(allOf(withId(R.id.form_usuario_nome_editText),
+                isDisplayed()))
+                .perform(replaceText("Felipe"),
+                        closeSoftKeyboard());
 
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(android.R.id.button1), withText("Adicionar"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.buttonPanel),
-                                        0),
-                                3)));
-        appCompatButton.perform(scrollTo(), click());
+        onView(allOf(withId(android.R.id.button1),
+                withText("Adicionar"),
+                isDisplayed()))
+                .perform(scrollTo(), click());
 
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.item_usuario_id_com_nome), withText("(1) Felipe"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.lista_usuario_recyclerview),
-                                        0),
-                                0),
-                        isDisplayed()));
-        textView.check(matches(withText("(1) Felipe")));
+        onView(allOf(withId(R.id.item_usuario_id_com_nome),
+                isDisplayed()))
+                .check(matches(withText("(1) Felipe")));
 
     }
 
@@ -109,24 +74,5 @@ public class ListaUsuariosTelaTest {
     private void limpaDeDadosInterno() {
         Context appContext = InstrumentationRegistry.getTargetContext();
         appContext.deleteDatabase(BuildConfig.BANCO_DE_DADOS);
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
