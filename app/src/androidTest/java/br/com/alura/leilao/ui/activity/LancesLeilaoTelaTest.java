06b4cded699a9b0b71b421c59402700178ccbd31
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2020-10-21 21:51:02 -0300

    aula 5.1, 5.2 - new test

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/LancesLeilaoTelaTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/LancesLeilaoTelaTest.java
@@ -136,4 +136,169 @@ public class LancesLeilaoTelaTest extends BaseTesteIntegracao {
                         isDisplayed())));
 
     }
+
+    @Test
+    public void deve_AtualizarLancesDoLeilao_QuandoReceberTresLances() throws IOException {
+        tentaSalvarLeilaoNaApi(new Leilao("Carro"));
+
+        mainActivity.launchActivity(new Intent());
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.alertTitle),
+                withText("Usuários não encontrados"),
+                isDisplayed()))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(android.R.id.message),
+                withText("Não existe usuários cadastrados! Cadastre um usuário para propor o lance."),
+                isDisplayed()))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(android.R.id.button1),
+                withText("Cadastrar usuário"),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.lista_usuario_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.form_usuario_nome_editText),
+                isDisplayed()))
+                .perform(click(),
+                        typeText("Felipe"),
+                        closeSoftKeyboard());
+
+        onView(allOf(withId(android.R.id.button1),
+                withText("Adicionar"),
+                isDisplayed()))
+                .perform(scrollTo(), click());
+
+        onView(allOf(withId(R.id.lista_usuario_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.form_usuario_nome_editText),
+                isDisplayed()))
+                .perform(click(),
+                        typeText("Evelyn"),
+                        closeSoftKeyboard());
+
+        onView(allOf(withId(android.R.id.button1),
+                withText("Adicionar"),
+                isDisplayed()))
+                .perform(scrollTo(), click());
+
+        pressBack();
+
+        //Propor 1
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.alertTitle),
+                withText("Novo lance"),
+                isDisplayed()))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(R.id.form_lance_valor_edittext),
+                isDisplayed()))
+                .perform(click(),
+                        typeText("200"),
+                        closeSoftKeyboard());
+
+        onView(allOf(withId(R.id.form_lance_usuario),
+                isDisplayed()))
+                .perform(click());
+
+        onData(is(new Usuario(1, "Felipe")))
+                .inRoot(isPlatformPopup())
+                .perform(click());
+
+        onView(allOf(withId(android.R.id.button1),
+                withText("Propor"),
+                isDisplayed()))
+                .perform(click());
+
+        //Propor 2
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.alertTitle),
+                withText("Novo lance"),
+                isDisplayed()))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(R.id.form_lance_valor_edittext),
+                isDisplayed()))
+                .perform(click(),
+                        typeText("300"),
+                        closeSoftKeyboard());
+
+        onView(allOf(withId(R.id.form_lance_usuario),
+                isDisplayed()))
+                .perform(click());
+
+        onData(is(new Usuario(2, "Evelyn")))
+                .inRoot(isPlatformPopup())
+                .perform(click());
+
+        onView(allOf(withId(android.R.id.button1),
+                withText("Propor"),
+                isDisplayed()))
+                .perform(click());
+
+        //Propor 3
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.alertTitle),
+                withText("Novo lance"),
+                isDisplayed()))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(R.id.form_lance_valor_edittext),
+                isDisplayed()))
+                .perform(click(),
+                        typeText("400"),
+                        closeSoftKeyboard());
+
+        onView(allOf(withId(R.id.form_lance_usuario),
+                isDisplayed()))
+                .perform(click());
+
+        onData(is(new Usuario(1, "Felipe")))
+                .inRoot(isPlatformPopup())
+                .perform(click());
+
+        onView(allOf(withId(android.R.id.button1),
+                withText("Propor"),
+                isDisplayed()))
+                .perform(click());
+
+        //Fazer assertion para as views de maior e menor lance, e tbm, para os maiores lances
+        FormatadorDeMoeda formatador = new FormatadorDeMoeda();
+        onView(withId(R.id.lances_leilao_maior_lance))
+                .check(matches(allOf(withText(formatador.formata(400)),
+                        isDisplayed())));
+
+        onView(withId(R.id.lances_leilao_menor_lance))
+                .check(matches(allOf(withText(formatador.formata(200)),
+                        isDisplayed())));
+
+        onView(withId(R.id.lances_leilao_maiores_lances))
+                .check(matches(allOf(withText("400.0 - (1) Felipe\n" +
+                                "300.0 - (2) Evelyn\n" +
+                                "200.0 - (1) Felipe\n"),
+                        isDisplayed())));
+
+    }
 }
